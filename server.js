71570472e1955ee37e7ad564652e6705652c9ca6
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2024-01-17 21:56:54 -0400

    FIXED - express static path Dev/Pub

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static('Develop/public'));
+
 
 
 //Make GET route for homepage from public 
